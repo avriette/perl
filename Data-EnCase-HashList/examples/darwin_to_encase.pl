77dd66617f68e9d09cbccc77336395f3359e968a
Alex Avriette <alex@cpan.org>
2013-09-04 17:06:56 -0400

    initial example (let's call this a unit test for now)

---
+++ Data-EnCase-HashList/examples/darwin_to_encase.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+use lib qw{ lib };
+
+use Data::EnCase::HashList;
+use Data::EnCase::HashList::Hash;
+use Data::Dumper;
+use File::Slurp;
+
+my @md5_lines = read_file( qw{ data/darwin_md5.txt } );
+
+my @objects = Data::EnCase::HashList::Hash->new_from_md5( @md5_lines );
+
+print Dumper( \@objects );
