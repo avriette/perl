03e6d4bfe73a148e35191dd4c89689f418316c35
Jane Avriette <jane@cpan.org>
2014-01-24 16:21:54 -0500

    this connects over ssl and authenticates via sasl just fine. <3

--- mudra/javascript/harness.js
+++ mudra/javascript/harness.js
@@ -1,9 +1,43 @@
-// this is the groundwork for the js client for mudra
+// this is the groundwork for the js message queue event generator for mudra
+
+/* Your sasl.json looks like:
+{
+	"saslpassword" : "i#like#22/7",
+	"saslnick"     : "mudra"
+}
+*/
+var config = require('./sasl.json');
 
 // instantiate the bot
 var irc = require('irc');
-var client = new irc.Client('irc.freenode.net', 'myNick', {
-	channels: ['##mudra'],
+var client = new irc.Client('irc.freenode.net', 'mudra', {
+	userName    : 'mudra',
+	realName    : 'this gigantic robot kills',
+	port        : 6697,
+	debug       : true,
+	showErrors  : true,
+	autoRejoin  : true,
+	autoConnect : true,
+	channels    : ['##mudra'],
+	stripColors : false,
+	channelPrefixes : "&#",
+	messageSplit : 512,
+
+	// these three lines refer to ssl
+	secure      : true,
+	selfSigned  : true, // basically give no fucks. it's irc.
+	certExpired : true, // basically give no fucks. it's irc.
+
+	// sasl auth -- see above re json file
+	sasl        : true,
+	userName    : config.saslnick,
+	password    : config.saslpassword
+
+	/*
+	floodProtection : false,
+	floodProtectionDelay : 1000,
+	*/
+
 });
 
 // let's add some event handlers
@@ -13,8 +47,7 @@ client.addListener('error', function(message) {
 	console.log('error: ', message);
 });
 
-// this is basically like _default from P::C::IRC, right?
+// the raw listener here can basically be ignored if debug is set to true above.
 client.addListener( 'raw', function(message) {
-	console.log( 'raw received: ' + message.command + ' ' + message.args + "\n" );
-	// mmmmmmhm.
+	// console.log( 'raw received: ' + message.command + ' ' + message.args + "\n" );
 });
